4d39f5d5bb2324e6ef8f4a844cbe1aef79225bb6
RajanSubramaniam <rajansaroj34@gmail.com>
2015-12-02 08:21:37 -0500

    displayed dynamic data from the database and adjusted the fonts

--- predictions/views.py
+++ predictions/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from predictions.models import Movie
 
 # Create your views here.
 def home(request):
-	return render(request, 'home.html')
+	movies = Movie.objects.filter(user_ratings=5)
+
+	return render(request, 'home.html',{
+		'movies':movies,
+	})
+
+
+
+
+
+
+
+
